b9ea0fa0ceb964e17f3e724ec9b68401469253b6
rajasaran256 <rajasaran.silambu@gmail.com>
2020-01-02 00:22:59 +0000

    initial commit

--- src/main/java/hello/BirthdayServiceImpl.java
+++ src/main/java/hello/BirthdayServiceImpl.java
@@ -6,18 +6,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BirthdayServiceImpl implements BirthdayService {
-
-	private static Logger logger = LoggerFactory.getLogger(BirthdayServiceImpl.class);
-
-	@Value("${file.path}")
-	private String filePath;
 
 	@Override
 	public String getUserBirthday(String userName) throws Exception {
@@ -26,13 +18,10 @@ public class BirthdayServiceImpl implements BirthdayService {
 		ObjectInputStream inputObjectStream = null;
 		HashMap<String, String> userBirthdayMap = null;
 
-		fis = new FileInputStream(filePath);
+		fis = new FileInputStream("birthdayObj.ser");
 		inputObjectStream = new ObjectInputStream(fis);
-
+		
 		Object object = inputObjectStream.readObject();
-
-		logger.debug("Username read operation from the object file is successfull");
-
 		userBirthdayMap = (HashMap<String, String>) object;
 
 		return userBirthdayMap.get(userName);
@@ -45,11 +34,10 @@ public class BirthdayServiceImpl implements BirthdayService {
 		FileOutputStream outputFile = null;
 		ObjectOutputStream outputStream = null;
 
-		outputFile = new FileOutputStream(filePath);
+		outputFile = new FileOutputStream("birthdayObj.ser");
 		outputStream = new ObjectOutputStream(outputFile);
 		outputStream.writeObject(userBirthdayMap);
-
-		logger.debug("Username write operation to the object file is successfull");
+		
 
 	}
 
